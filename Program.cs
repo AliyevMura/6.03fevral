414588fbe970856ccc1b18636ee52cd2c6ca2e5e
AliyevMura <7lvghl9@code.edu.az>
2023-03-06 21:55:57 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Task1
+//int year = 2013;
+
+//if (year + 18 < 2023)
+//{
+//    Console.WriteLine("18 yashiniz olub");
+//}
+//else
+//{
+//    Console.WriteLine("18 yashiniz tamam olmayib");
+//}
+
+
+
+
+//Task3
+//int number = 321, qaliq, sum = 0;
+//while(number>0)
+//{
+//   qaliq = number % 10;
+//    sum = sum + qaliq;
+//number = number / 10;
+//}
+//Console.WriteLine("reqemlerin cemi {0}" ,sum);
+
+
